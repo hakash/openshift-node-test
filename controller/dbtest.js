2e8ac737ff2dd6c1070dcfad85959b14ad42b25e
Morten Olsrud <post@dittdesign.com>
2017-11-16 14:44:16 +0100

    .

--- controller/dbtest.js
+++ controller/dbtest.js
@@ -2,7 +2,7 @@ var express = require("express");
 var MongoClient = require("mongodb").MongoClient;
 var router = express.Router();
 
-router.get("/dbtest", function(req, res, next){
+router.get("/", function(req, res, next){
 	// Connect to the db
 	MongoClient.connect("mongodb://mongodb.test.svc:27017/exampleDb", function(err, db) {
 		if(!err) {
